1ff8ee1376d23d4dcca933317f3a510c92b5f801
dhyoga <dhyogap@gmail.com>
2025-03-02 23:10:08 +0700

    add some fix

--- components/About.tsx
+++ components/About.tsx
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <div className="w-screen md:w-[600px] bg-black/60 px-2 md:px-0 md:ml-4 rounded-lg">
       <div className="rounded-lg shadow-md py-2 md:py-4 px-3">
-        <h1 className="md:text-3xl text-xl mb-1 font-semibold italic">
+        <h1 className="md:text-3xl text-xl mb-1 font-semibold">
           Remindeen
         </h1>
         <hr />
@@ -12,13 +12,7 @@ const About = () => {
             className="cursor-pointer w-24 h-24 md:w-36 md:h-36 rounded-md hover:drop-shadow-[0_0_2em_#3c9934e0] transition duration-300 ease-in-out"
           />
           <p className="text-justify text-xs md:text-sm">
-            Remindeen is a browser extension specifically designed to serve as a
-            reminder and source of inspiration for Muslims in their daily
-            activities. Its main feature is an accurate prayer schedule based on
-            the user's location, ensuring that worship is always performed on
-            time. Additionally, Remindeen displays short Islamic teachings in
-            the form of selected Quranic verses and hadiths that provide
-            motivation and bring peace to the heart.
+          Remindeen is a browser extension specifically designed to be a prayer reminder and source of inspiration for Muslims in carrying out their daily activities.
           </p>
         </div>
       </div>
--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -4,22 +4,17 @@ import React from "react";
 const Popup: React.FC = () => {
   return (
     <div className="px-3 text-center">
-      <h1>About</h1>
+      <h1>SerdaduCode</h1>
       <img
         src="/icon/serdadu.png"
         width={80}
         height={60}
         className="logo round"
       />
-      <p className="my-2">Reach Your Dream, Rich Your Mind</p>
+      <p className="my-2 text-bold text-sub">Reach Your Dream, Rich Your Mind</p>
       <hr />
       <p className="textJustify mx-2">
-        This IT community focuses on skill development through collaboration,
-        training and knowledge sharing. With a commitment to open source
-        research and development projects, our goal is to expand our portfolio
-        and build a reputation in the technology industry. We believe that the
-        best innovations come from collaboration and the spirit of sharing is
-        the cornerstone of every initiative.
+      SerdaduCode is a research team focused on skill development through collaboration, training, and open-source projects. We aim to grow our portfolio while making a positive impact in the tech industry.
       </p>
     </div>
   );
